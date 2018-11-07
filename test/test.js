a4e227a524ccc8e0c68da45165ab65b16522c222
Nitin Jotwani <nitinj@iconnectsolutions.com>
2018-11-07 18:40:15 +0530

    mocha

---
+++ test/test.js
@@ -0,0 +1,8 @@
+describe('Addition Tests', function() {
+  it('should return 3 when passed one and two', function() {
+    var numOne = 1;
+    var numTwo = 2;
+    var expectedResult = 3;
+    var actualResult = Add(numOne, numTwo);
+  });
+});
