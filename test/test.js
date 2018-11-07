65dcc0eccf76e0e8cea08ef778aa3aab83df76b3
Nitin Jotwani <nitinj@iconnectsolutions.com>
2018-11-07 19:08:36 +0530

    using-chai-should

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
+var should = require('chai').should();
+var add = require('../maths');
+
 describe('Addition Tests', function() {
   it('should return 3 when passed one and two', function() {
     var numOne = 1;
     var numTwo = 2;
     var expectedResult = 3;
-    var actualResult = Add(numOne, numTwo);
+    var actualResult = add(numOne, numTwo);
+
+    actualResult.should.equal(expectedResult);
+  });
+
+  it('should not return 3 when passed one and four', function() {
+    var numOne = 1;
+    var numTwo = 4;
+    var expectedResult = 3;
+    var actualResult = add(numOne, numTwo);
+
+    actualResult.should.not.equal(expectedResult);
   });
 });
